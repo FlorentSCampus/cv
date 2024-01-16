ddffc2819a09e53a24537782ab6365434c37a544
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-16 09:22:35 +0100

    content

---
+++ src/content.php
@@ -0,0 +1,96 @@
+<main>
+        <h1 class="text-primary text-center mt-5 mb-5">Étudiant DevOps</h1>
+        <div class="banner-profile g-0 row ps-5 pe-5">
+            <div class="desc-profile col-12 col-sm-8 col-lg-10 d-flex flex-column justify-content-center">
+                <h2 class="text-primary mt-5"> Florent SARTOUT </h2>
+                <p class="mb-5"> 655, grande rue <br>
+                    26300 Barbières <br>
+                    florent-sartout@hotmail.fr
+                </p>
+            </div>
+            <div class="col-12 col-sm-4 col-lg-2 d-flex align-items-center">
+                <img class="img-fluid rounded-4" src="img/florent_sartout.webp" alt="Photo de profil">
+            </div>
+        </div>
+
+        <section class="mt-5 mb-5">
+            <div class="ms-5 me-5">
+                <h3 class="text-primary text-center mb-3"> PRÉSENTATION </h3>
+                <div>
+                    <p class="text-center p-4 m-0 shadow rounded-4 bg-dark-subtle border border-primary"> Je suis à la
+                        recherche d'une alternance à partir de Septembre 2024
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="mb-5">
+            <div class="row g-0 ps-5 pe-5">
+                <div class="col-12 mb-3 d-flex flex-column align-items-center">
+                    <h3 class="text-primary text-center mb-3">COMPÉTENCES</h3>
+                    <ul>
+                        <li>HTML, CSS/SASS, JS, PHP, MySQL</li>
+                        <li>Illustrator, InDesign, Photoshop</li>
+                        <li>SketchUp</li>
+                    </ul>
+                </div>
+                <div class="col-12 d-flex flex-column align-items-center">
+                    <h3 class="text-primary text-center mb-3">HOBBIES</h3>
+                    <ol>
+                        <li>Musiques</li>
+                        <li>Jeux Vidéos</li>
+                        <li>Voyages</li>
+                    </ol>
+                </div>
+            </div>
+        </section>
+
+        <div class="row p-5 ms-4 mb-5 me-4 border border-primary rounded-4">
+            <div class="col-12 col-md-6 mb-5">
+                <div>
+                    <h3 class="text-primary text-center mb-4">PROFESSIONNEL</h3>
+                    <p class="mb-5">
+                        <span class="fw-bold">2021 | 2023</span><br>
+                        <span>Graphiste / Modélisateur 3D</span><br>
+                        <span>Champs-sur-Marne</span><br><br>
+                        <span>Capacité à créer des modèles tridimensionnels précis en utilisant SketchUp. Concevoir des environnements virtuels (architecture). Sens artistique développé pour créer des designs esthétiquement plaisants et fonctionnels.
+                        </span>
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        <span class="fw-bold">2015 | 2020</span><br>
+                        <span>Graphiste / Webdesigner</span><br>
+                        <span>Paris</span><br><br>
+                        <span>Créer des éléments graphiques attractifs tels que logos, icônes, images, illustrations et mises en page pour les sites web. Élaborer des maquettes et des wireframes pour planifier l'architecture visuelle des pages web avant le développement. Cela inclut généralement la disposition des éléments et la hiérarchie visuelle. Travailler en étroite collaboration avec les développeurs web pour s'assurer que les conceptions peuvent être mises en œuvre efficacement et correspondent aux exigences techniques. Avoir des connaissances de base en HTML et CSS peut être un avantage pour comprendre comment les conceptions sont mises en œuvre sur le web.
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div>
+                    <h3 class="text-primary text-center mb-4">FORMATION</h3>
+                    <p class="mb-5">
+                        <span class="fw-bold">2012 | 2015</span><br>
+                        <span>BTS Industries Graphiques – Études de réalisation de produits de communication</span><br>
+                        <span>Paris</span><br><br>
+                        <span>Le technicien en Études de Réalisation d’un Projet de Communication doit être ouvert au monde de la communication,
+                        témoigner de qualités d’analyse, de synthèse, d’organisation, être curieux et créatif pour s’adapter aux évolutions
+                        technologiques et en saisir les opportunités.</span>
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        <span class="fw-bold">2008 | 2012</span><br>
+                        <span>Bac Pro RPIP</span><br>
+                        <span>Paris</span><br><br>
+                        <span>Le Bac Pro prépare au métier d’imprimeur qui consiste à reproduire textes et images sur un support papier (journaux,
+                        livres, affiches,…). Le Bac Pro production imprimée vise à former des professionnels hautement qualifiés, conduisant des
+                        machines à système complexe pilotées à partir de pupitres informatisés (presses offset ou numérique polychrome).
+                        L’utilisation d’une machine d’impression complexe (une presse quatre couleurs par exemple), exige, vigilance, compétence
+                        et sens des responsabilités.</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
