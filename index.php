01924f1ddab8bf07c8d22bb3b4579d9fc8113815
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-16 09:16:04 +0100

    index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Florent SARTOUT, étudiant DevOps, est à la recherche d'une alternance à partir du début du mois de septembre 2024">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV - FSA</title>
+
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php require_once 'src/header.php' ?>
+    <?php require_once 'src/content.php' ?>
+    <?php require_once 'src/footer.php' ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
